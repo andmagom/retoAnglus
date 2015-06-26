7f0154d62660e44c732f4846574af5d1fdd55974
Andres M. Gomez <andres86mauricio@hotmail.com>
2015-06-26 00:10:34 -0500

    Ya se muestran los animales, se han incluido los archivos de manera local sin embargo hay que tener en cuenta que al parecer se  realizarian muchas peticiones al servidor para obtener las imagenes aun cuando solo se realize la peticion del json una única vez

--- public_html/js/game/game.js
+++ public_html/js/game/game.js
@@ -1,86 +1,140 @@
 
-var CANVAS_WIDTH = 480;
+var CANVAS_WIDTH = 800;
 var CANVAS_HEIGHT = 320;
 var FPS = 30;
-var imgPlayer="img/game/player.png";
+var imgPlayer = "img/game/player.png";
+var arrayAnimalsJson = [];
+var animalsSprite = sjs.List();
 
 
-var scene = sjs.Scene({w:CANVAS_WIDTH, h:CANVAS_HEIGHT,parent:document.getElementById("game1")});
 
+var scene = sjs.Scene({w: CANVAS_WIDTH, h: CANVAS_HEIGHT, parent: document.getElementById("game1")});
+var input  = scene.Input();
 
 setInterval(function () {
+    collision();
     update();
     draw();
-}, 1000 / FPS);
+}, 1000/FPS );
 
 function draw() {
-    player.draw();
+    drawAnimals();
+     player.draw();
 }
 
 function update() {
     player.update();
 }
 
+function collision()
+{
+    var collision = player.sprite.collidesWithArray(animalsSprite);
+    if(collision)
+    {
+        //alert("Ah chocado con: "+collision);
+        player.x=0;
+        player.y=0;
+        player.draw();
+    }
+}
+
 var player = {
-    x: CANVAS_WIDTH/2,
-    y: CANVAS_HEIGHT/2,
-    sprite : scene.Sprite(imgPlayer),
+    x: 0,
+    y: 0,
+    sprite: scene.Sprite(imgPlayer),
     draw: function () {
         this.sprite.update();
     },
     update: function ()
     {
-        var right=false;
+        var right = false;
         var left = false;
-        if (keydown.left) {
+  
+        if (input.keyboard.left) {
             player.x -= 2;
-            left=true;
+            left = true;
             this.sprite.setAngle(Math.PI);
         }
-        if (keydown.right) {
+        if (input.keyboard.right) {
             player.x += 2;
-            right=true;
+            right = true;
             this.sprite.setAngle(0);
         }
-        if (keydown.up) {
+        if (input.keyboard.up) {
             player.y -= 2;
-            if(left || right)
+            if (left || right)
             {
-                if(left)
+                if (left)
                 {
-                    this.sprite.setAngle(5*Math.PI/4);
-                }else
+                    this.sprite.setAngle(5 * Math.PI / 4);
+                } else
                 {
-                    this.sprite.setAngle(7*Math.PI/4);
+                    this.sprite.setAngle(7 * Math.PI / 4);
                 }
-            }else
+            } else
             {
-                this.sprite.setAngle(3*Math.PI/2);
+                this.sprite.setAngle(3 * Math.PI / 2);
             }
-            
+
         }
-        if (keydown.down) {
+        if (input.keyboard.down) {
             player.y += 2;
-            if(left || right)
+            if (left || right)
             {
-                if(left)
+                if (left)
                 {
-                    this.sprite.setAngle(3*Math.PI/4);
-                }else
+                    this.sprite.setAngle(3 * Math.PI / 4);
+                } else
                 {
-                    this.sprite.setAngle(Math.PI/4);
+                    this.sprite.setAngle(Math.PI / 4);
                 }
-            }else
+            } else
             {
-                this.sprite.setAngle(Math.PI/2);
+                this.sprite.setAngle(Math.PI / 2);
             }
-            
+
         }
         //Limito que el jugador no pueda salir del borde del juego.
         player.x = player.x.clamp(0, CANVAS_WIDTH - this.sprite.w);
-        player.y = player.y.clamp(7, CANVAS_HEIGHT - this.sprite.w+6);
+        player.y = player.y.clamp(7, CANVAS_HEIGHT - this.sprite.w + 6);
         this.sprite.setX(player.x);
         this.sprite.setY(player.y);
-        
+
     }
 };
+
+
+function createAnimals()
+{
+    var xAni=10;
+    $.each(arrayAnimalsJson, function ( ) {
+        var name = this.name;
+        var img = this.image;
+        var animal =  scene.Sprite(img);
+        animalsSprite.add(animal);
+        xAni+=50;
+    });
+}
+;
+
+function drawAnimals() {
+    var x = 10;
+    var ani;
+    while (ani = animalsSprite.iterate()) {
+        ani.setX(x);
+        ani.setY(100);
+        ani.update();
+        x += 170;
+    }
+
+}
+;
+
+$(document).ready(function () {
+    $.getJSON("json/data.json", function (data) {
+        arrayAnimalsJson = data.words;
+        createAnimals();
+        drawAnimals();
+    });
+
+});
